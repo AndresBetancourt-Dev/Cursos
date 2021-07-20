332cbb84c572fbb786699d8537e80a2529c9ad71
Andrés David Betancourt Santana <65095693+AndresBetancourt-Dev@users.noreply.github.com>
2021-07-20 16:05:54 -0500

    Curso Lodash - 100% Completion

---
+++ Curso Lodash/Array.js
@@ -0,0 +1,80 @@
+/* Chunk 
+Returns a chunk of the Array within specified N
+*/
+let groupOne = ["Andrés", "Maria", "Luis", "Carolina", "Catalina", "Santiago"];
+let groupTwo = ["Andrés", "Catalina", "Santiago"];
+let groupOfPeople = [
+  { firstName: "Andrés", age: 21 },
+  { firstName: "Maria", age: 21 },
+  { firstName: "Hernando", age: 21 },
+  { firstName: "Mary", age: 21 },
+  { firstName: "Hershey", age: 21 },
+  { firstName: "G", age: 21 },
+];
+
+let groupOfPeopleTwo = [
+  { firstName: "Andrés", age: 21 },
+  { firstName: "Maria", age: 20 },
+  { firstName: "Hernando", age: 23 },
+  { firstName: "Mary", age: 24 },
+  { firstName: "Hershey", age: 25 },
+  { firstName: "G", age: 21 },
+];
+
+console.table(_.chunk(groupOne, 2));
+console.table(_.chunk(groupOne, 3));
+console.table(_.chunk(groupOne, 4));
+
+/*
+
+Compact 
+
+*/
+
+let groupOfNulls = [null, undefined, 0, 1, true, false, [], {}, "Andrés"];
+
+console.warn(_.compact(groupOfNulls)); //Excludes null, undefined 0 and false
+
+/*
+
+Concat 
+
+*/
+
+console.log(_.concat(groupOfNulls, groupOne, groupOne, groupOne));
+
+/*  Difference */
+
+console.log(_.difference(groupOne, groupTwo));
+console.log(_.differenceBy(groupOfPeople, groupOfPeopleTwo, "firstName"));
+
+console.log(_.drop(groupOne, 5));
+console.log(_.drop(groupOfPeople, 3));
+console.log(_.dropRight(groupOne, 5));
+console.log(_.dropRight(groupOfPeople, 3));
+
+var users = [
+  { user: "barney", active: true },
+  { user: "fred", active: false },
+  { user: "pebbles", active: false },
+  { user: "barney", active: true },
+  { user: "barney", active: true },
+];
+
+console.table(
+  _.dropRightWhile(users, function (o) {
+    return !o.active;
+  })
+);
+// => objects for ['barney']
+
+const CONSTANT = 100;
+console.table(_.fill(Array(10), CONSTANT));
+
+console.log(users[_.findIndex(users, (e) => e.user === "barney")]);
+
+{
+  let thearray = [1, [2, [3, [4]], 5]];
+
+  console.log(_.flattenDepth(thearray, 2));
+}
